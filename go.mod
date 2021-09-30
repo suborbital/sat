ae0bff924a5bc21fff44df0bf01132caaac1f193
cohix <connor@suborbital.dev>
2021-09-30 08:52:42 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/suborbital/sat
+
+go 1.17
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/suborbital/grav v0.4.1
+	github.com/suborbital/reactr v0.12.0
+	github.com/suborbital/vektor v0.5.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/schollz/peerdiscovery v1.6.1 // indirect
+	github.com/sethvargo/go-envconfig v0.3.2 // indirect
+	github.com/wasmerio/wasmer-go v1.0.4 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ sat/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/suborbital/grav/discovery/local"
+	"github.com/suborbital/grav/grav"
+	"github.com/suborbital/grav/transport/websocket"
+	"github.com/suborbital/reactr/request"
+	"github.com/suborbital/reactr/rt"
+	"github.com/suborbital/reactr/rwasm"
+	"github.com/suborbital/vektor/vk"
+)
+
+type sat struct {
+	r *rt.Reactr
+	v *vk.Server
+	g *grav.Grav
+}
+
+func main() {
+	args := os.Args
+	modulePath := args[0]
+	runnableName := strings.TrimSuffix(filepath.Base(modulePath), ".wasm")
+
+	// choose a random port above 1000
+	randPort := rand.Intn(10000) + 1000
+
+	r := rt.New()
+	t := websocket.New()
+	g := grav.New(
+		grav.UseTransport(t),
+		grav.UseDiscovery(local.New()),
+		grav.UseEndpoint(fmt.Sprintf("%d", randPort), "/meta/message"),
+	)
+
+	exec := r.Register(runnableName, rwasm.NewRunner(modulePath), rt.Autoscale(0))
+	r.Listen(g.Connect(), runnableName)
+
+	v := vk.New(
+		vk.UseAppName(runnableName),
+		vk.UseHTTPPort(randPort),
+	)
+
+	v.HandleHTTP(http.MethodGet, "/meta/message", t.HTTPHandlerFunc())
+
+	v.POST("/", func(r *http.Request, ctx *vk.Ctx) (interface{}, error) {
+		req, err := request.FromVKRequest(r, ctx)
+		if err != nil {
+			ctx.Log.Error(errors.Wrap(err, "failed to FromVKRequest"))
+			return nil, vk.E(http.StatusInternalServerError, "unknown error")
+		}
+
+		result, err := exec(req).Then()
+		if err != nil {
+			ctx.Log.Error(errors.Wrap(err, "failed to exec"))
+			return nil, vk.Wrap(http.StatusTeapot, err)
+		}
+
+		return result, nil
+	})
+
+	sat := &sat{
+		r: r,
+		v: v,
+		g: g,
+	}
+
+	if err := sat.serve(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s *sat) serve() error {
+	return s.v.Start()
+}
