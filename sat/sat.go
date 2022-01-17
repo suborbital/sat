2c9815833f648f78253bd6622cb2d0c0f9afa9e5
cohix <connor@suborbital.dev>
2022-01-16 22:28:14 -0500

    max out Reactr threads at 8

--- sat/sat.go
+++ sat/sat.go
@@ -68,7 +68,7 @@ func New(config *Config) (*Sat, error) {
 	exec.Register(
 		config.JobType,
 		runner,
-		rt.Autoscale(0),
+		rt.Autoscale(8),
 		rt.MaxRetries(0),
 		rt.RetrySeconds(0),
 		rt.PreWarm(),
