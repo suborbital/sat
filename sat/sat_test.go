553026dbc36c1189473f60a2dbddb64c2543210d
cohix <connor@suborbital.dev>
2022-01-21 15:27:21 -0500

    add additional tests

--- sat/sat_test.go
+++ sat/sat_test.go
@@ -2,6 +2,7 @@ package sat
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -24,6 +25,58 @@ func TestEchoRequest(t *testing.T) {
 
 	resp.AssertStatus(200)
 	resp.AssertBodyString("hello my friend")
+}
+
+func Test405Request(t *testing.T) {
+	sat, err := satForFile("../examples/hello-echo/hello-echo.wasm")
+	if err != nil {
+		t.Error(errors.Wrap(err, "failed to satForFile"))
+		return
+	}
+
+	vt := vtest.New(sat.testServer())
+
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+
+	resp := vt.Do(req, t)
+
+	fmt.Println(string(resp.Body))
+
+	resp.AssertStatus(405)
+}
+
+func TestErrorRequest(t *testing.T) {
+	sat, err := satForFile("../examples/return-err/return-err.wasm")
+	if err != nil {
+		t.Error(errors.Wrap(err, "failed to satForFile"))
+		return
+	}
+
+	vt := vtest.New(sat.testServer())
+
+	req, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte{}))
+
+	resp := vt.Do(req, t)
+
+	resp.AssertStatus(401)
+	resp.AssertBodyString(`{"status":401,"message":"don't go there"}`)
+}
+
+func TestPanicRequest(t *testing.T) {
+	sat, err := satForFile("../examples/panic-at-the-disco/panic-at-the-disco.wasm")
+	if err != nil {
+		t.Error(errors.Wrap(err, "failed to satForFile"))
+		return
+	}
+
+	vt := vtest.New(sat.testServer())
+
+	req, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte{}))
+
+	resp := vt.Do(req, t)
+
+	resp.AssertStatus(500)
+	resp.AssertBodyString(`{"status":500,"message":"unknown error"}`)
 }
 
 func satForFile(filepath string) (*Sat, error) {
