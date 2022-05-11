142df0dc39c6d652da0af2022501caed69c4a58a
cohix <connor@suborbital.dev>
2022-05-11 10:04:51 -0400

    move embedded example into examples dir

--- examples/embedded/main.go
+++ examples/embedded/main.go
@@ -7,12 +7,15 @@ import (
 	"github.com/suborbital/sat/sat"
 )
 
-// Set the following environment variables before running
+// If you are using a control plane server, set the following environment variables before running
 // 	SAT_CONTROL_PLANE={Control Plane URL}
 // 	SAT_ENV_TOKEN={Environment token}
 //
-// Replace Fully Qualified Function Name in sat.ConfigFromRunnableArg to load a function
+// If you are using a control plane server, pass the FQFN as the Runnable Arg (like below)
 // See also https://docs.suborbital.dev/compute/concepts/fully-qualified-function-names
+//
+// If you are NOT using a control plane server, pass the path to the .wasm file on disk you'd like to load
+//
 func main() {
 	config, _ := sat.ConfigFromRunnableArg("com.suborbital.acmeco#default::embed@v1.0.0")
 
