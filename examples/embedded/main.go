3635daec9a6d53223fc44c38d93b2afc5c792e0b
Connor Hicks <connor@suborbital.dev>
2022-05-11 10:09:36 -0400

    feat(sat): allow easy embedding (#101)
    * allow Sat to be embedded in Go programs
    
    * Add NoopTracer and instructions to example
    
    * fall back to noop tracer
    
    * move embedded example into examples dir
    
    Co-authored-by: Ryan Pridgeon <ryan.n.pridgeon@gmail.com>

---
+++ examples/embedded/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/suborbital/sat/sat"
+)
+
+// If you are using a control plane server, set the following environment variables before running
+// 	SAT_CONTROL_PLANE={Control Plane URL}
+// 	SAT_ENV_TOKEN={Environment token}
+//
+// If you are using a control plane server, pass the FQFN as the Runnable Arg (like below)
+// See also https://docs.suborbital.dev/compute/concepts/fully-qualified-function-names
+//
+// If you are NOT using a control plane server, pass the path to the .wasm file on disk you'd like to load
+//
+func main() {
+	config, _ := sat.ConfigFromRunnableArg("com.suborbital.acmeco#default::embed@v1.0.0")
+
+	s, _ := sat.New(config, nil)
+
+	for i := 1; i < 100; i++ {
+		resp, err := s.Exec([]byte("world!"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s\n", resp.Output)
+	}
+}
